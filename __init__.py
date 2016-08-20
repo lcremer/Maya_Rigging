00e10ce93bb906f53eb01f9a0c6d9f6e1c2930a9
Leo <Leonard.Cremer@gmail.com>
2016-08-19 22:36:01 -0500

    Setup MainMenu drop injection

--- __init__.py
+++ __init__.py
@@ -4,3 +4,18 @@ import Rigging
 import Controls
 import Core
 import UI
+import Maya_UtilLib
+import pymel.core as pm
+from functools import partial
+
+
+def MenuItem():
+    pm.menuItem(parent='CustomTools', subMenu=True, tearOff=True, label='Rigging')
+    pm.menuItem(label='Auto Rig', command=partial(UI.AutoRig.Open))
+
+
+def AddMenuItem():
+    Maya_UtilLib.Menu.AddModuleMenu(module='Maya_Rigging', menuFunc=MenuItem)
+    Maya_UtilLib.Menu.Draw()
+
+AddMenuItem()
