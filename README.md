cbd1874c369f93aea3254812e0066a406b42c610
lcremer <leonard.cremer>
2018-12-03 18:34:08 -0800

    testing alternate method for reloading modules.

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ def Loader():
 	except:
 		pass
 	import Maya_UtilLib
-	Maya_UtilLib.Menu.Draw()
+	Maya_UtilLib.Menu.draw()
 
 utils.executeDeferred('Loader()')
 #--------------------------------------------------------------------------------
--- __init__.py
+++ __init__.py
@@ -8,13 +8,15 @@ import Maya_UtilLib
 import pymel.core as pm
 from functools import partial
 
+
 def MenuItem():
-    pm.menuItem(parent='CustomTools', subMenu=True, tearOff=True, label='Rigging')
+    pm.menuItem(parent=Maya_UtilLib.ui_name, subMenu=True, tearOff=True, label='Rigging')
     pm.menuItem(label='Auto Rig', command=partial(Maya_Rigging.UI.AutoRig.Open))
 
+
 def AddMenuItem():
-    Maya_UtilLib.Menu.AddModuleMenu(module='Maya_Rigging', menuFunc=MenuItem)
-    Maya_UtilLib.Menu.Draw()
+    Maya_UtilLib.Menu.add_module_menu(module='Maya_Rigging', menu_func=MenuItem)
+    Maya_UtilLib.Menu.draw()
 
 
 AddMenuItem()
